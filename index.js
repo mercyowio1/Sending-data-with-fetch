589ff6d015421561cf8f27cc44481727d6dd7a02
Mercy <owinomercy983@gmail.com>
2025-03-30 14:53:23 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const body = document.querySelector("body");
+      const idElement = document.createElement("p");
+      idElement.textContent = `New User ID: ${data.id}`;
+      body.appendChild(idElement);
+    })
+    .catch((error) => {
+      const body = document.querySelector("body");
+      const errorElement = document.createElement("p");
+      errorElement.textContent = `Error: ${error.message}`;
+      body.appendChild(errorElement);
+    });
+}
